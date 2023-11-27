11d3a3aa1c4f22ba38b69e86ad973becf2d1c907
ANTKHRV <mambaneed@bk.ru>
2023-11-27 23:45:23 +0300

    28

--- src/Content/Str1.jsx
+++ src/Content/Str1.jsx
@@ -24,15 +24,35 @@ const Str1 = () => {
 
                 <div className={a.divleft112}>
                 <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft2}>Составляйте расписание сейчас, а оплачивайте перед началом урока</div></div>
-                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft3}>Низкие цены за высокие результаты. От 590 рублей за урок</div></div>
+                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft3}>Пошаговая подготовка учеников для максимальных баллов по ОГЭ</div></div>
             </div>
             <div className={a.divleft113}>
-            <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft4}>Подготовка учеников на максимальные баллы по ОГЭ</div></div>
-            <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft5}>Интерактивные занятия на уникальной платформе</div></div>
+            <div className={`${a.divleft22} ${a.fff}`}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft4}>Хорошие цены за высокие результаты. От 590 рублей за урок</div></div>
+            <div className={`${a.divleft22} ${a.fff}`}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft5}>Интерактивные занятия на уникальной платформе</div></div>
             </div> 
             </div>
             <div className={a.divleft6}>Попробовать бесплатно</div>
             </div>
+
+            <div className={a.divleft0}>
+            <div className={a.divleft1}>
+                <a>ИНДИВИДУАЛЬНЫЕ ОНЛАЙН-УРОКИ С</a> 
+                <a className={a.divleft11}>ОПЫТНЫМ ПЕДАГОГОМ</a>
+                </div>
+                <div className={a.divleft111}>
+
+                <div className={a.divleft112}>
+                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft2}>Составляйте расписание сейчас, а оплачивайте перед началом урока</div></div>
+                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft3}>Пошаговая подготовка учеников для максимальных баллов по ОГЭ</div></div>
+            </div>
+            <div className={a.divleft113}>
+            <div className={`${a.divleft22} ${a.fff}`}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft4}>Хорошие цены за высокие результаты. От 590 рублей за урок</div></div>
+            <div className={`${a.divleft22} ${a.fff}`}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft5}>Интерактивные занятия на уникальной платформе</div></div>
+            </div> 
+            </div>
+            <div className={a.divleft6}>Попробовать бесплатно</div>
+            </div>
+
 
             <div className={a.divright}>.</div>
             </div>
