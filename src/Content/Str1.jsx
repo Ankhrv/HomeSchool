4350abfe7724c068fbe1aae4337107c0f89b006a
ANTKHRV <mambaneed@bk.ru>
2023-12-01 01:09:59 +0300

    1dec

--- src/Content/Str1.jsx
+++ src/Content/Str1.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import a from './Str1.module.css';
 import logo from './y.png';
+import logo100 from './1.png';
+import logo101 from './3.png';
+import logo102 from './2.png';
 
 
 
@@ -11,23 +14,25 @@ const Str1 = () => {
 
 <div className={a.A}>
         <div className={a.a}>
-
+       <div className={a.fon}>
+           
+          
             <div className={a.div1}>
 
             <div className={a.divleft}>
             <div className={a.divleft1}>
-                <a>ИНДИВИДУАЛЬНЫЕ</a> 
-                <a>ОНЛАЙН-УРОКИ С</a>
+                <a>АКТУАЛЬНЫЕ</a> 
+                <a>ВИДЕО-УРОКИ С</a>
                 <a className={a.divleft11}>ОПЫТНЫМ ПЕДАГОГОМ</a>
                 </div>
                 <div className={a.divleft111}>
 
                 <div className={a.divleft112}>
-                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft2}>Составляйте расписание сейчас, а оплачивайте перед началом урока</div></div>
-                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft3}>Пошаговая подготовка учеников для максимальных баллов по ОГЭ</div></div>
+                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft2}>Составляйте cписок тем сейчас, или после консультации педагога</div></div>
+                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft3}>Пошаговая подготовка учеников, максимальные знания для ГИА</div></div>
             </div>
             <div className={a.divleft113}>
-            <div className={`${a.divleft22} ${a.fff}`}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft4}>Хорошие цены за высокие результаты. От 590 рублей за урок</div></div>
+            <div className={`${a.divleft22} ${a.fff}`}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft4}>Хорошие цены за высокие результаты. От 390 рублей за урок</div></div>
             <div className={`${a.divleft22} ${a.fff}`}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft5}>Интерактивные занятия на уникальной платформе</div></div>
             </div> 
             </div>
@@ -36,27 +41,29 @@ const Str1 = () => {
 
             <div className={a.divleft0}>
             <div className={a.divleft1}>
-                <a>ИНДИВИДУАЛЬНЫЕ ОНЛАЙН-УРОКИ С</a> 
-                <a className={a.divleft11}>ОПЫТНЫМ ПЕДАГОГОМ</a>
+                <a>АКТУАЛЬНЫЕ ВИДЕО-УРОКИ </a> 
+                <a className={a.divleft11}>С ОПЫТНЫМ ПЕДАГОГОМ</a>
                 </div>
                 <div className={a.divleft111}>
 
                 <div className={a.divleft112}>
-                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft2}>Составляйте расписание сейчас, а оплачивайте перед началом урока</div></div>
-                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft3}>Пошаговая подготовка учеников для максимальных баллов по ОГЭ</div></div>
+                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft2}>Составляйте cписок тем сейчас, или после консультации педагога</div></div>
+                <div className={a.divleft22}><img src={logo} alt='logo'className={a.t}></img> <div className={a.divleft3}>Пошаговая подготовка учеников, максимальные знания для ГИА</div></div>
             </div>
             <div className={a.divleft113}>
-            <div className={`${a.divleft22} ${a.fff}`}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft4}>Хорошие цены за высокие результаты. От 590 рублей за урок</div></div>
+            <div className={`${a.divleft22} ${a.fff}`}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft4}>Хорошие цены за высокие результаты. От 390 рублей за урок</div></div>
             <div className={`${a.divleft22} ${a.fff}`}><img src={logo} alt='logo'className={a.t}></img><div className={a.divleft5}>Интерактивные занятия на уникальной платформе</div></div>
             </div> 
             </div>
             <div className={a.divleft6}>Попробовать бесплатно</div>
             </div>
 
-
+            <div className={a.divrighttt}>
             <div className={a.divright}>.</div>
-            </div>
-        </div>
+            <div className={a.divrightt}>Как в реальной жизни. Только лучше!</div>
+            </div></div>
+           
+        </div></div>
         </div>
         </div>
 
