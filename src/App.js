4d0a02e0fc0df52453b22fdb3fbbf17dbc1410c1
ANTKHRV <mambaneed@bk.ru>
2023-12-06 03:27:41 +0300

    6dec

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Header from './Components/Header/Header';
 // import Main from './Components/Menu/Main/Main';
-import Str1 from './Content/Str1';
+import Str1 from './Content/Str1/Str1';
+import Str2 from './Content/Str2/Str2';
 import Menu from './Components/Menu/Menu';
 import Ball from './Components/Menu/Balll/Ball';
 import Footer from './Components/Footer/Footer';
@@ -13,6 +14,7 @@ function App() {
     
     <Header/>
      <Str1/>
+     <Str2/>
     <div className='Menu'>
     <Menu/>
     <Ball/>
---
+++ src/Content/Str2/Str2.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import c from './Str2.module.css';
+
+
+
+
+
+const Str2 = () => {
+    return (
+<div className={c.A1}>
+
+<div className={c.A}>
+<div className={c.fon}>
+        <div className={c.a}>
+        <div className={c.Textt}> 
+<div className={c.Text}>
+<a className={c.Text2}>Найдём</a>
+<a className={c.Text1}>решение</a>
+<a className={c.Text2}>для</a>
+<a className={c.Text2}>любых</a>
+<a className={c.Text2}>школьных</a>
+<a className={c.Text1}>задач</a>
+</div>
+
+<div className={c.downtext}><div className={c.downtext1}>Школа – непростое испытание для детей и их родителей. Хотите, чтобы после окончания школы остались</div> <div className={c.downtext2}>только хорошие воспоминания и достойные результаты? Тогда доверьте обучение опытным педагогам.</div></div>
+<div className={c.downtext11}>Школа – непростое испытание для детей и их родителей. Хотите, чтобы после окончания школы остались только хорошие воспоминания и достойные результаты? Тогда доверьте обучение опытным педагогам.</div> 
+
+</div> 
+ 
+
+        </div>
+        </div>
+        </div>
+
+        <div className={c.scrollmenu}>
+<div className={c.aa2}><div className={c.b1}><div className={c.b2}></div><div className={c.b22}>Подготовка к ОГЭ</div><div className={c.b3}>Разберём специфику заданий, научим алгоритмам быстрых и правильных решений. От вас практика — от меня знания.</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
+<div className={c.aa}><div className={c.b1}><div className={c.b20}></div><div className={c.b22}>Подготовка к ГИА</div><div className={c.b3}>Разберём специфику заданий, научим алгоритмам быстрых и правильных решений. От вас практика — от меня знания.</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
+<div className={c.aa}><div className={c.b1}><div className={c.b21}></div><div className={c.b22}>Средняя школа</div><div className={c.b3}>В этот период успеваемость падает у 70% учеников. Поможем вернуть интерес к учёбе и уверенность в своих знаниях.</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
+<div className={c.aa3}><div className={c.b1}><div className={c.b211}></div><div className={c.b22}>Срочная помощь</div><div className={c.b3}>Сложности могут возникнуть в любой момент. Несколько занятий с нашим педагогом помогут избежать эффекта «снежного кома».</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
+</div>     
+
+        </div>
+        
+        );
+}
+
+export default Str2;
