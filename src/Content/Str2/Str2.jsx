ac7debc44f937093b33fab1f51d9a24928f147d8
ANTKHRV <mambaneed@bk.ru>
2023-12-08 07:21:52 +0300

    8dec

--- src/Content/Str2/Str2.jsx
+++ src/Content/Str2/Str2.jsx
@@ -39,11 +39,13 @@ const Str2 = () => {
         </div>
 
         <div className={c.scrollmenu}>
+       
 <div className={c.aa2}><div className={c.b1}><div className={c.b2}></div><div className={c.b22}>Подготовка к ОГЭ</div><div className={c.b3}>Разбор тем по заданиям. Знакомство c алгоритмами быстрых решений. Задания на дом и практика, проверка заданий.</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
 <div className={c.aa}><div className={c.b1}><div className={c.b20}></div><div className={c.b22}>Подготовка к КР</div><div className={c.b3}>Разберём контрольные работы по разным программам. Практическое задание на дом и проверка, для закрепления результата.</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
 <div className={c.aa}><div className={c.b1}><div className={c.b21}></div><div className={c.b22}>Средняя школа</div><div className={c.b3}>Много новых предметов и падает успеваемость. Видео-уроки помогут вернуть интерес к учёбе и уверенность в своих знаниях.</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
 <div className={c.aa3}><div className={c.b1}><div className={c.b211}></div><div className={c.b22}>Срочная помощь</div><div className={c.b3}>Сложности в домашнем задании? Хочешь понять тему, а не списать? Смотри видео-уроки по заданиям с учебников.</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
 </div>     
+    
 
         </div>
         
--- src/Content/Str3/Str2.jsx
+++ src/Content/Str3/Str2.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import c from './Str2.module.css';
+import c from './Str3.module.css';
 import { Helmet } from 'react-helmet';
 
 
 
 
-const Str2 = () => {
+const Str3 = () => {
     return (
         
 <div className={c.A1}>
@@ -20,34 +20,20 @@ const Str2 = () => {
         <div className={c.a}>
         <div className={c.Textt}> 
 <div className={c.Text}>
-<a className={c.Text2}>Найдём</a>
-<a className={c.Text1}>решение</a>
-<a className={c.Text2}>для</a>
-<a className={c.Text2}>любых</a>
-<a className={c.Text2}>школьных</a>
-<a className={c.Text1}>задач</a>
+<a className={c.Text2}>Мы знаем, как поднять</a>
+<a className={c.Text2}>репетиторство на </a>
+<a className={c.Text1}>новый уровень</a>
+</div>
+</div>
+</div>
+</div>
+</div>
 </div>
 
-<div className={c.downtext}><div className={c.downtext1}>Школа – непростое испытание для детей и их родителей. Хотите, чтобы после окончания школы остались</div> <div className={c.downtext2}>только хорошие воспоминания и достойные результаты? Тогда доверьте обучение опытныму педагогу.</div></div>
-<div className={c.downtext11}>Школа – непростое испытание для детей и их родителей. Хотите, чтобы после окончания школы остались только хорошие воспоминания и достойные результаты? Тогда доверьте обучение опытному педагогу.</div> 
 
-</div> 
- 
 
-        </div>
-        </div>
-        </div>
-
-        <div className={c.scrollmenu}>
-<div className={c.aa2}><div className={c.b1}><div className={c.b2}></div><div className={c.b22}>Подготовка к ОГЭ</div><div className={c.b3}>Разбор тем по заданиям. Знакомство c алгоритмами быстрых решений. Задания на дом и практика, проверка заданий.</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
-<div className={c.aa}><div className={c.b1}><div className={c.b20}></div><div className={c.b22}>Подготовка к КР</div><div className={c.b3}>Разберём контрольные работы по разным программам. Практическое задание на дом и проверка, для закрепления результата.</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
-<div className={c.aa}><div className={c.b1}><div className={c.b21}></div><div className={c.b22}>Средняя школа</div><div className={c.b3}>Много новых предметов и падает успеваемость. Видео-уроки помогут вернуть интерес к учёбе и уверенность в своих знаниях.</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
-<div className={c.aa3}><div className={c.b1}><div className={c.b211}></div><div className={c.b22}>Срочная помощь</div><div className={c.b3}>Сложности в домашнем задании? Хочешь понять тему, а не списать? Смотри видео-уроки по заданиям с учебников.</div><div className={c.strr1}><div className={c.str1}></div></div></div></div>
-</div>     
-
-        </div>
         
         );
 }
 
-export default Str2;
+export default Str3;
