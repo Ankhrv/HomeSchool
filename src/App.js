c69ff885c211a89e227cc4b16961e7bfec328fb3
ANTKHRV <mambaneed@bk.ru>
2023-12-09 04:30:28 +0300

    9dec

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Components/Header/Header';
 // import Main from './Components/Menu/Main/Main';
 import Str1 from './Content/Str1/Str1';
 import Str2 from './Content/Str2/Str2';
+import Str3 from './Content/Str3/Str3';
 import Menu from './Components/Menu/Menu';
 import Ball from './Components/Menu/Balll/Ball';
 import Footer from './Components/Footer/Footer';
@@ -15,6 +16,7 @@ function App() {
     <Header/>
      <Str1/>
      <Str2/>
+     <Str3/>
     <div className='Menu'>
     <Menu/>
     <Ball/>
---
+++ src/Content/Str3/Str3.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import c from './Str3.module.css';
+import { Helmet } from 'react-helmet';
+
+
+
+
+const Str3 = () => {
+    return (
+        
+<div className={c.A1}>
+<Helmet>
+        <title>Домашняя школа</title>
+        <meta name="description" content="Актуальные видео-уроки от опытного педагога" />
+       
+      </Helmet>
+
+<div className={c.A}>
+<div className={c.fon}>
+        <div className={c.a}>
+        <div className={c.Textt}> 
+<div className={c.Text}>
+<a className={c.Text2}>Мы знаем, как поднять</a>
+<a className={c.Text2}>репетиторство на </a>
+<a className={c.Text1}>новый уровень</a>
+</div>
+</div>
+</div>
+
+<div className={c.blocks}>
+<div className={c.block1}><div className={c.block111}><div className={c.block102}></div><div className={c.block12}>Технологии на 5+</div><div className={c.block122}>Простая и понятная онлайн-платформа, ведь мы не только лидеры в образовании, но и резиденты Сколково.</div></div></div>
+<div className={c.block2}><div className={c.block111}><div className={c.block103}></div><div className={c.block12}>Всё по науке</div><div className={c.block122}>Сервис на базе главного образовательного проекта для учителей: только лучшие педагоги.</div></div></div>
+<div className={c.block3}><div className={c.block111}><div className={c.block104}></div><div className={c.block12}>Комфортная оплата</div><div className={c.block122}>Оплачивайте сразу или в рассрочку: распланируйте занятия на месяц вперёд или платите поурочно.</div></div></div>
+<div className={c.block4}><div className={c.block111}><div className={c.block105}></div><div className={c.block12}>Персональный подход</div><div className={c.block122}>Качество и доступность: добивайтесь лучших результатов, занимаясь индивидуально.</div></div></div>
+
+</div>
+<div className={c.blocks1}>
+<div className={c.blockK1}><div className={c.block102}></div><div className={c.block102K}></div><div className={c.block12}>Технологии на 5+</div><div className={c.block122}>Простая и понятная онлайн-платформа, ведь мы не только лидеры в образовании, но и резиденты Сколково.</div></div>
+<div className={c.blockK2}><div className={c.block103}></div><div className={c.block103K}></div><div className={c.block12}>Всё по науке</div><div className={c.block122}>Сервис на базе главного образовательного проекта для учителей: только лучшие педагоги.</div></div>
+<div className={c.blockK3}><div className={c.block104}></div><div className={c.block104K}></div><div className={c.block12}>Комфортная оплата</div><div className={c.block122}>Оплачивайте сразу или в рассрочку: распланируйте занятия на месяц вперёд или платите поурочно.</div></div>
+<div className={c.blockK4}><div className={c.block105}></div><div className={c.block105K}></div><div className={c.block12}>Персональный подход</div><div className={c.block122}>Качество и доступность: добивайтесь лучших результатов, занимаясь индивидуально.</div></div>
+</div></div>
+</div>
+</div>
+
+
+
+
+        
+        );
+}
+
+export default Str3;
